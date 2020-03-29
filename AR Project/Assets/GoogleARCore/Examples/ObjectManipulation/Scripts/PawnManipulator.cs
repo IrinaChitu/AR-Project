367ac9a7597bab5e71a7babf00ad2b05c13d1082
Teodor <teodormarchitan@gmail.com>
2020-03-29 23:08:34 +0300

    Remove useless things

--- AR Project/Assets/GoogleARCore/Examples/ObjectManipulation/Scripts/PawnManipulator.cs
+++ AR Project/Assets/GoogleARCore/Examples/ObjectManipulation/Scripts/PawnManipulator.cs
@@ -20,100 +20,112 @@
 
 namespace GoogleARCore.Examples.ObjectManipulation
 {
-    using GoogleARCore;
-    using UnityEngine;
+	using GoogleARCore;
+	using UnityEngine;
 
-    /// <summary>
-    /// Controls the placement of objects via a tap gesture.
-    /// </summary>
-    public class PawnManipulator : Manipulator
-    {
-        /// <summary>
-        /// The first-person camera being used to render the passthrough camera image (i.e. AR
-        /// background).
-        /// </summary>
-        public Camera FirstPersonCamera;
+	/// <summary>
+	/// Controls the placement of objects via a tap gesture.
+	/// </summary>
+	public class PawnManipulator : Manipulator
+	{
+		/// <summary>
+		/// The first-person camera being used to render the passthrough camera image (i.e. AR
+		/// background).
+		/// </summary>
+		public Camera FirstPersonCamera;
 
-        /// <summary>
-        /// A prefab to place when a raycast from a user touch hits a plane.
-        /// </summary>
-        public GameObject PawnPrefab;
+		/// <summary>
+		/// A prefab to place when a raycast from a user touch hits a plane.
+		/// </summary>
+		public GameObject PawnPrefab;
 
-        /// <summary>
-        /// Manipulator prefab to attach placed objects to.
-        /// </summary>
-        public GameObject ManipulatorPrefab;
+		/// <summary>
+		/// Manipulator prefab to attach placed objects to.
+		/// </summary>
+		public GameObject ManipulatorPrefab;
 
-        /// <summary>
-        /// Returns true if the manipulation can be started for the given gesture.
-        /// </summary>
-        /// <param name="gesture">The current gesture.</param>
-        /// <returns>True if the manipulation can be started.</returns>
-        protected override bool CanStartManipulationForGesture(TapGesture gesture)
-        {
-            if (gesture.TargetObject == null)
-            {
-                return true;
-            }
+		/// <summary>
+		/// Returns true if the manipulation can be started for the given gesture.
+		/// </summary>
+		/// <param name="gesture">The current gesture.</param>
+		/// <returns>True if the manipulation can be started.</returns>
+		protected override bool CanStartManipulationForGesture(TapGesture gesture)
+		{
+			if (gesture.TargetObject == null)
+			{
+				return true;
+			}
 
-            return false;
-        }
+			return false;
+		}
 
-        /// <summary>
-        /// Function called when the manipulation is ended.
-        /// </summary>
-        /// <param name="gesture">The current gesture.</param>
-        protected override void OnEndManipulation(TapGesture gesture)
-        {
-            if (gesture.WasCancelled)
-            {
-                return;
-            }
+		/// <summary>
+		/// Function called when the manipulation is ended.
+		/// </summary>
+		/// <param name="gesture">The current gesture.</param>
+		protected override void OnEndManipulation(TapGesture gesture)
+		{
+			if (gesture.WasCancelled)
+			{
+				return;
+			}
 
-            // If gesture is targeting an existing object we are done.
-            if (gesture.TargetObject != null)
-            {
-                return;
-            }
+			// If gesture is targeting an existing object we are done.
+			if (gesture.TargetObject != null)
+			{
+				return;
+			}
 
-            // Raycast against the location the player touched to search for planes.
-            TrackableHit hit;
-            TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon;
+			// Raycast against the location the player touched to search for planes.
+			TrackableHit hit;
+			TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon;
 
-            if (Frame.Raycast(
-                gesture.StartPosition.x, gesture.StartPosition.y, raycastFilter, out hit))
-            {
-                // Use hit pose and camera pose to check if hittest is from the
-                // back of the plane, if it is, no need to create the anchor.
-                if ((hit.Trackable is DetectedPlane) &&
-                    Vector3.Dot(FirstPersonCamera.transform.position - hit.Pose.position,
-                        hit.Pose.rotation * Vector3.up) < 0)
-                {
-                    Debug.Log("Hit at back of the current DetectedPlane");
-                }
-                else
-                {
-                    // Instantiate game object at the hit pose.
-                    var gameObject = Instantiate(PawnPrefab, hit.Pose.position, hit.Pose.rotation);
+			if (Frame.Raycast(
+				gesture.StartPosition.x, gesture.StartPosition.y, raycastFilter, out hit))
+			{
+				// Use hit pose and camera pose to check if hittest is from the
+				// back of the plane, if it is, no need to create the anchor.
+				if ((hit.Trackable is DetectedPlane) &&
+					Vector3.Dot(FirstPersonCamera.transform.position - hit.Pose.position,
+						hit.Pose.rotation * Vector3.up) < 0)
+				{
+					Debug.Log("Hit at back of the current DetectedPlane");
+				}
+				else
+				{
+					Debug.Log(GlobalScript.selectedObject);
+					// Mesh mesh = Resources.Load(GlobalScript.selectedObject, Mesh);
+					// PawnPrefab.GetComponent<MeshFilter>().mesh = mesh;
+					// Debug.Log((GameObject)Resources.Load(GlobalScript.selectedObject));
+					PawnPrefab = Instantiate(Resources.Load(GlobalScript.selectedObject, typeof(GameObject))) as GameObject;
+					Debug.Log(PawnPrefab);
 
-                    // Instantiate manipulator.
-                    var manipulator =
-                        Instantiate(ManipulatorPrefab, hit.Pose.position, hit.Pose.rotation);
+					// Instantiate game object at the hit pose.
+					// PawnPrefab = (GameObject)Instantiate(Resources.Load(GlobalScript.selectedObject));
+					var gameObject = Instantiate(PawnPrefab, hit.Pose.position, hit.Pose.rotation);
+					// Debug.Log(Resources.Load(GlobalScript.selectedObject);
 
-                    // Make game object a child of the manipulator.
-                    gameObject.transform.parent = manipulator.transform;
+					// GameObject gameObject = (GameObject)Instantiate(Resources.Load(GlobalScript.selectedObject), hit.Pose.position, hit.Pose.rotation);
+					// Debug.Log(gameObject);
 
-                    // Create an anchor to allow ARCore to track the hitpoint as understanding of
-                    // the physical world evolves.
-                    var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+					// Instantiate manipulator.
+					var manipulator =
+						Instantiate(ManipulatorPrefab, hit.Pose.position, hit.Pose.rotation);
 
-                    // Make manipulator a child of the anchor.
-                    manipulator.transform.parent = anchor.transform;
+					// Make game object a child of the manipulator.
+					gameObject.transform.parent = manipulator.transform;
 
-                    // Select the placed object.
-                    manipulator.GetComponent<Manipulator>().Select();
-                }
-            }
-        }
-    }
+					// Create an anchor to allow ARCore to track the hitpoint as understanding of
+					// the physical world evolves.
+					var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+
+					// Make manipulator a child of the anchor.
+					manipulator.transform.parent = anchor.transform;
+
+					// Select the placed object.
+					manipulator.GetComponent<Manipulator>().Select();
+				}
+			}
+		}
+	}
 }
---
+++ AR Project/Assets/TableMenuScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TableMenuScript : MonoBehaviour
+{
+	public void OpenObjectManipulationScene()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
